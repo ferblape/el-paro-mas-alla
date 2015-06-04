75e05e96492dbbc1d5ff89ac746901abad849f63
martínez <mtnezb@gmail.com>
2015-06-04 16:25:42 +0200

    temporary line chart

--- assets/js/line_chart.js
+++ assets/js/line_chart.js
@@ -1,7 +1,7 @@
 var Lines = function(session){
 
   var margin = {top: 80, right: 140, bottom: 70, left: 60},
-    width = 750 - margin.left - margin.right,
+    width = 950 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 
   var parseDate = d3.time.format("%Y").parse;
@@ -13,7 +13,8 @@ var Lines = function(session){
       .range([height, 0]);
 
   var color = d3.scale.ordinal()
-              .range(["#BD2D28", "#E3BA22", "#708259", "#E6842A", "#137B80", "#8E6C8A", "#b9c1ab"]);
+              .range(["#BD2D28", "#E3BA22", "#708259", "#E6842A", "#137B80", "#8E6C8A", "#b9c1ab"])
+              .domain(["parados", "inactivos", "ocupados", "activos", "asalariados", "c_propia", "asal_temp", "asal_temp_parc", "asal_temp_parc_inv", "asal_temp_comp", "asal_indef", "asal_indef_parc", "asal_indef_parc_inv", "asal_indef_comp", "para_b_4mas_a", "para_b_2a4a", "para_b_1a2a", "para_b_6ma1a", "para_b_menos6m", "inac_desanim"]);
 
   var xAxis = d3.svg.axis()
       .scale(x)
@@ -25,9 +26,8 @@ var Lines = function(session){
       .tickSubdivide(0)
       .tickSize(0-width);
 
-
   var line = d3.svg.line()
-      .interpolate("linear")
+      .interpolate("basis")
       .x(function(d) { return x(d.year); })
       .y(function(d) { return y(d.porcentaje); });
 
@@ -36,6 +36,12 @@ var Lines = function(session){
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var line_charts = {
+    ocupados:   ["ocupados", "asal_indef", "asal_temp", "asal_temp_parc", "asal_temp_parc_inv", "asal_temp_comp", "asal_indef_parc", "asal_indef_parc_inv", "asal_indef_comp"],
+    parados:    ["parados", "para_b_4mas_a", "para_b_2a4a", "para_b_1a2a", "para_b_6ma1a", "para_b_menos6m"],
+    inactivos:  ["inactivos", "inac_desanim", "inactivos"],
+  };
 
   d3.csv("assets/data/df_per.csv", function(error, rawData) {
 
@@ -49,22 +55,33 @@ var Lines = function(session){
     var situations = d3.set(rawData.map(function(d) { return d.situation; })).values();
 
     // Set up the domains
-    color.domain(situations)
     x.domain(d3.extent(rawData, function(d) { return d.year; }));
     y.domain([0, 1]);
    
 
     data = rawData
-              .filter(function(d) { return d.codigo == session.get('autonomousRegion'); })
-              .filter(function(d) { return d.edad == session.get('age'); });
+              .filter(function(d) { return d.edad == session.get('age'); })
+              .filter(function(d) { return d.sexo == session.get('sex'); })
+              .filter(function(d) { return d.situation != "total"; });
 
     data.sort(function(a,b){
       return a.year - b.year
     });
 
-    var nested_data = d3.nest()
+    
+    var data_ccaa = data
+                      .filter(function(d) { return d.codigo == session.get('autonomousRegion'); });
+
+    var nested_data_ccaa = d3.nest()
                         .key(function(d) { return d.situation; })
-                        .entries(data);
+                        .entries(data_ccaa);
+    var data_esp = data
+                    .filter(function(d) { return d.codigo === "0"; });
+
+    var nested_data_esp = d3.nest()
+                        .key(function(d) { return d.situation; })
+                        .entries(data_esp);
+
 
     // Draw the axis  
     svgLines.append("g")
@@ -84,28 +101,51 @@ var Lines = function(session){
         .style("fill", "black")
         .text("%"); 
 
-    // Draw the lines
-    var lines = svgLines.selectAll(".lines")
-        .data(nested_data)
-      .enter().append("g")
-        .attr("class", "lines");
+  console.log(color.range())
+  console.log(color.domain())
+
+  // Draw the lines 
+  var lines_ccaa = svgLines.selectAll(".lines.ccaa")
+      .data(nested_data_ccaa)
+    .enter().append("g")
+      .attr("class", "lines ccaa");
 
 
-  lines.append("path")
-      .attr("class", "line total")
-      .attr("id", function(d){ return d.situation;})
-      .attr("d", function(d) { console.log(d.values);return line(d.values); })
+  lines_ccaa.append("path")
+      .attr("class", "line ccaa")
+      .attr("id", function(d){ return d.key;})
+      .attr("d", function(d) { return line(d.values); })
       .style('fill', 'none')
-      // .style("opacity", function(d) {
-      //   if (d.situation === "parados" || d.situation === "ocupados") {
-      //     return 1;
-      //   } else {
-      //     return 0;
-      //   }
-      //  })
-      .style("stroke", function(d) { return d3.rgb(color(d.situation)).darker(.5); })
-      .style("stroke-width", function(d) {return d.situation !== "ocupados" ? 3 : 4})
-      .style("stroke-dasharray",  function(d) {return d.situation !== "ocupados" ? ("0,0") : ("10,10");});
+      .style("opacity", function(d) {
+        if (d.key === "parados" || d.key === "ocupados" || d.key === "inactivos") {
+          return 1;
+        } else {
+          return 0;
+        }
+       })
+      .style("stroke", function(d) { return d3.rgb(color(d.key)).darker(.5); })
+      .style("stroke-width", function(d) {return d.key !== "ocupados" ? 2 : 3});
+
+  var lines_esp = svgLines.selectAll(".lines.esp")
+      .data(nested_data_esp)
+    .enter().append("g")
+      .attr("class", "lines esp");
+
+  lines_esp.append("path")
+      .attr("class", "line esp")
+      .attr("id", function(d){ return d.key;})
+      .attr("d", function(d) { return line(d.values); })
+      .style('fill', 'none')
+      .style("opacity", function(d) {
+        if (d.key === "parados" || d.key === "ocupados" || d.key === "inactivos") {
+          return 1;
+        } else {
+          return 0;
+        }
+       })
+      .style("stroke", function(d) { return d3.rgb(color(d.key)).darker(.5); })
+      .style("stroke-width", 2)
+      .style("stroke-dasharray", ("8,8"));
 
   });
 }
