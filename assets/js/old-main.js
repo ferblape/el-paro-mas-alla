6b285f590eab9f958bc5d16a3018e11824384454
Fernando Blat <ferblape@gmail.com>
2015-06-05 14:53:47 +0200

    Details

--- assets/js/old-main.js
+++ assets/js/old-main.js
@@ -39,7 +39,7 @@ $(function(){
     $('#autonomy-text').html(autonomiesTexts['a' + session.get('autonomousRegion')]);
 
     $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top + 70
+      scrollTop: $($(this).attr('href')).offset().top
     }, 200);
   });
 
